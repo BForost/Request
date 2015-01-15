231aef74f68e59f63cbc74b331a8a3968e47d046
Foroster <GForoster@gmail.com>
2015-01-15 16:31:36 +0300

    publish

---
+++ dbo/Tables/Request.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Request] (
+    [IdRequestGUID] UNIQUEIDENTIFIER NOT NULL,
+    [IdRequest]     VARCHAR (200)    NOT NULL,
+    [Priority]      INT              NOT NULL,
+    [Date]          DATE             NOT NULL,
+    [Latitude]      VARCHAR (100)    NOT NULL,
+    [Longitude]     VARCHAR (100)    NOT NULL,
+    [Address]       VARCHAR (500)    NOT NULL,
+    PRIMARY KEY CLUSTERED ([IdRequestGUID] ASC)
+);
